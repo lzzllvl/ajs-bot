8c5b539253422f50673a2fb899f6e82295c57499
Greg Lee <gml2136@gmail.com>
2017-12-10 13:11:46 -0800

    sub service

--- hooks/utils/translator.js
+++ hooks/utils/translator.js
@@ -8,6 +8,7 @@ const keywords = {
 
 module.exports = (messageBody) => {
     let found = Object.keys(keywords).filter((val) => {
+        console.log(RegExp(`\b${val}\b `, 'i'))
         return messageBody.match(RegExp(`\b${val}\b `, 'i'))
     })
     console.log(keywords[found[0]])
