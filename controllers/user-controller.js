ca3228860499f1456fcfeaa57e967daf6487d677
Greg Lee <gml2136@gmail.com>
2017-12-09 23:00:13 -0800

    sub service

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,13 +9,29 @@ module.exports = {
         })
     },
     setCurrentJoke: (username, jokeId) => {
-        return User.findOneAndUpdate({
-            username: username
-        }, {
-            $set: {
-                currentJoke: jokeId
-            }
-        })
+        //both of these need to happen before we can move on
+        return new Promise((resolve, reject) => 
+            Promise.all([
+                //event 1
+                User.findOneAndUpdate({
+                    username: username
+                }, {
+                    $set: {
+                        currentJoke: jokeId
+                    }
+                }),
+                //event a
+                User.findOneAndUpdate({
+                    username: username 
+                }, {
+                    $inc: {
+                        jokesToday: 1
+                    }
+                })
+            ])
+            .then(([thingOne, thingTwo]) => resolve(thingOne))
+            .catch(err => reject(err))
+
     },
     getCurrentPunchline: (username) => {
         return new Promise((resolve, reject) => {
@@ -25,16 +41,13 @@ module.exports = {
             .then(user => {
                 Joke.findOne({
                     _id: user.currentJoke
-                }).then((data) => {
+                }).then((data) => { 
                     User.findOneAndUpdate({
                         username: username
                     }, {
                         $push: {
                             jokesSeen: data._id
-                        },
-                        $inc: {
-                            jokesToday: 1
-                        }
+                    }
                     }).exec((err, res) => {
                         err ? reject(err): resolve(data)
                     })
@@ -48,7 +61,7 @@ module.exports = {
                 username: username
             }).then((data) => {
                 if(data.jokesToday >= 3) { //4 jokes a day
-                    resolve({ noJokeMessage: true})
+                    resolve({ noJokeMessage: true })
                 } else {
                     Joke.findOne({
                         _id: {
@@ -60,7 +73,7 @@ module.exports = {
                     
                 }
             })
-            .catch(err => {reject(err)})
+            .catch(err => reject(err))
         })   
     },
     subscribe: (username) => {
